3a94c37ac22cd9cc81c23739da725564162867ff
satotune <satotune2718@gmail.com>
2019-07-08 11:23:13 +0900

    受信データのチェック

--- UARTtoSPI.ino
+++ UARTtoSPI.ino
@@ -1,53 +1,50 @@
-nclude <SPI.h>
-#include <stdint.h>
+#include <SPI.h>
 
-// モータのビットデータ
-uint64_t InitData[]{
-        0b111111111011000000010111100000000000000000000000,
-        0b111111111011000000010000000001100000000000000000,
-        0b111111111011000000010000100000000001011110000000,
-        0b111111111011000000010001000001010000000010010000,
-        0b111111111011000000010001100000000000000100111000,
-        0b111111111011000000010001100001000000000001001000,
-        0b111111111011000000010001100010000000000100111000,
-        0b111111111011000000010001100011000000000001001000,
-        0b111111111011000000010010000001010000000000001000,
-};
+#define BYTE_SIZE 6
 
-uint64_t MoveData[]{
-        0b111111111011000000000111111000111111011111111000,
-        0b111111111011000000000111111000100000000000001000,
+const uint8_t test_data_set[2][BYTE_SIZE] = {
+        {
+                0b11111111,
+                0b10110000,
+                0b00000111,
+                0b11100011,
+                0b11110111,
+                0b11111000,
+        },
+        {
+                0b11111111,
+                0b10110000,
+                0b00000111,
+                0b11100010,
+                0b00000000,
+                0b00001000,
+        }
 };
 
 void setup() {
     Serial.begin(9600);
+    // SPI通信のセットアップ
     pinMode(SS, OUTPUT); //デジタル10番ピンをOUTPUTに設定
     digitalWrite(SS, HIGH); //デジタルの10番ピンからHighを出力
-    SPI.setBitOrder(MSBFIRST); // 下位ビットから送信
+    SPI.setBitOrder(MSBFIRST); // 最上位ビットから送信
     SPI.setClockDivider(SPI_CLOCK_DIV8); //分周器1/8を指定 同期クロック16/8=2MHz
-    SPI.setDataMode(SPI_MODE3); //アイドル時Low、立上がりエッジでデータ取得
+    SPI.setDataMode(SPI_MODE2); //アイドル時HIGH、立上がりエッジでデータ取得
     SPI.begin(); //SPI通信の初期化、有効化
 }
 
 bool Flg = true;
 void loop() {
+    // １回のみ実行
     if (Flg) {
         Flg = false;
-        // 制御するデバイスに通信の開始を通知する
-        digitalWrite(SS, LOW);
-        for (uint64_t data:InitData) {
-            SPI.transfer(&data, 6);
-            delay(10); //必要に応じて設定
+        for (auto send_data:test_data_set) {
+            digitalWrite(SS, LOW);
+            for (auto i = 0; i < BYTE_SIZE; i++) {
+                uint8_t recv_data = SPI.transfer(send_data[i]);
+                Serial.println(recv_data, BIN);
+            }
+            digitalWrite(SS, HIGH);
+            delay(10);
         }
-        for (uint64_t data:MoveData) {
-            SPI.transfer(&data, 6);
-            delay(10); //必要に応じて設定
-        }
-        // 制御するデバイスに通信の終了を通知する
-        digitalWrite(SS, HIGH);
     }
 }
-
-
-
-
